f3f92ae40737b4ad592b28c961025a37870b0392
Anastasiya <nastylia@gmail.com>
2016-09-27 17:41:09 +0300

    completed 1, 2, 6, 7

--- railway/db/schema.rb
+++ railway/db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914082543) do
+ActiveRecord::Schema.define(version: 20160927135158) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "railway_stations_routes", force: :cascade do |t|
+    t.integer "railway_station_id"
+    t.integer "route_id"
   end
 
   create_table "routes", force: :cascade do |t|
@@ -26,8 +31,12 @@ ActiveRecord::Schema.define(version: 20160914082543) do
 
   create_table "trains", force: :cascade do |t|
     t.integer  "number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "route_id"
+    t.integer  "current_station_id"
+    t.index ["current_station_id"], name: "index_trains_on_current_station_id"
+    t.index ["route_id"], name: "index_trains_on_route_id"
   end
 
 end
