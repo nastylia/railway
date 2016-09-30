a5e5d4ac081b5ba1210b12d5d38e7ebc52f99f35
Anastasiya <nastylia@gmail.com>
2016-09-30 19:15:07 +0300

    fixed comments

--- railway/app/models/train.rb
+++ railway/app/models/train.rb
@@ -4,4 +4,14 @@ class Train < ApplicationRecord
   has_many :tickets
 
   has_many :carriages
+
+  def count_seats(carriages)
+    result = {up: 0, down: 0}
+    return result unless carriages
+    carriages.each do |carriage|
+      result[:up] += carriage.up_seats
+      result[:down] += carriage.down_seats
+    end
+    result
+  end
 end
