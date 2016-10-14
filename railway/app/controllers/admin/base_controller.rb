ccc97ed3a26024ed25a2b47a75a13328094d2b09
Anastasiya <nastylia@gmail.com>
2016-10-14 23:14:35 +0300

    deleted alert, added flash for notice

--- railway/app/controllers/admin/base_controller.rb
+++ railway/app/controllers/admin/base_controller.rb
@@ -5,6 +5,7 @@ class Admin::BaseController < ApplicationController
   protected
 
   def check_admin
-    redirect_to root_path, alert "Not enought rights to view this page" unless current_user.admin?
+    flash[:notice] = "Not enought rights to view this page"
+    redirect_to root_path unless current_user.admin?
   end
 end
