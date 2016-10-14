3298baf13ab48357f95ffd935649ef985e6bfc2b
Anastasiya <nastylia@gmail.com>
2016-10-14 13:36:57 +0300

    added deleted string to base_controller

--- railway/app/controllers/admin/base_controller.rb
+++ railway/app/controllers/admin/base_controller.rb
@@ -5,6 +5,6 @@ class Admin::BaseController < ApplicationController
   protected
 
   def check_admin
-    redirect_to root_path unless current_user.admin?
+    redirect_to root_path, alert "Not enought rights to view this page" unless current_user.admin?
   end
 end
