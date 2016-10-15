0722ee3404229ba3a25ddd3b117e31c00f412cd6
Anastasiya <nastylia@gmail.com>
2016-10-16 02:50:17 +0300

    added inline editing for routes

---
+++ railway/app/assets/javascripts/routes.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $('a.edit_route').click(function(e) {
+    e.preventDefault();
+
+    var route_id;
+    var form;
+    var title;
+
+    route_id = $(this).data('routeId');
+    console.log(route_id);
+
+    form = $('#edit_route_' + route_id);
+    title = $('#route_title_' + route_id);
+
+    if (!$(this).hasClass('cancel')) {
+      $(this).html('Cancel');
+      $(this).addClass('cancel');
+    } else {
+      $(this).html('Edit');
+      $(this).removeClass('cancel');
+    }
+
+    form.toggle();
+    title.toggle();
+  });
+});
